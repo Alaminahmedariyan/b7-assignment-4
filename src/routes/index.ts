a368c895c7179cb5b31dfc513487183fe3be67c3
Alamin Ahmed <alaminahmedariyan2022@gmail.com>
2026-07-07 07:58:32 +0600

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,30 @@
+// import { Router } from "express";
+
+
+// const router = Router();
+
+// interface IModuleRoute {
+//   path: string;
+//   route: Router;
+// }
+
+// const moduleRoutes: IModuleRoute[] = [
+//   {
+//     path: "/users",
+//     route: UserRoutes,
+//   },
+//   {
+//     path: "/posts",
+//     route: PostRoutes,
+//   },
+//   {
+//     path: "/comments",
+//     route: CommentRoutes,
+//   },
+// ];
+
+// moduleRoutes.forEach((route) => {
+//   router.use(route.path, route.route);
+// });
+
+// export default router;
